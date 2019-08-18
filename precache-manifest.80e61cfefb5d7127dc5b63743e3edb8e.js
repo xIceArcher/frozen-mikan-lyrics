f22789343f4be833d6274dfa3000ed3cc9691692
nianfei97 <tehnianfei@gmail.com>
2019-08-18 20:39:02 +0800

    Updates

---
+++ precache-manifest.80e61cfefb5d7127dc5b63743e3edb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3caca578866ef5535a78043c770682b",
+    "url": "/frozen-mikan-lyrics/index.html"
+  },
+  {
+    "revision": "a46d350db2762e21afd5",
+    "url": "/frozen-mikan-lyrics/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "e858e03d965b230c6676",
+    "url": "/frozen-mikan-lyrics/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "a46d350db2762e21afd5",
+    "url": "/frozen-mikan-lyrics/static/js/2.ec04fa75.chunk.js"
+  },
+  {
+    "revision": "e858e03d965b230c6676",
+    "url": "/frozen-mikan-lyrics/static/js/main.b4a94a38.chunk.js"
+  },
+  {
+    "revision": "65504e87b1a56ec2dbff",
+    "url": "/frozen-mikan-lyrics/static/js/runtime~main.e50f76b8.js"
+  }
+]);
